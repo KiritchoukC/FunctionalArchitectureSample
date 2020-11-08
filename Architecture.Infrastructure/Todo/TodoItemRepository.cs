1579aa629511e97f4e49bc13b0faac5400c0e8b2
KiritchoukC <clement.kiritchouk@gmail.com>
2020-11-08 14:46:01 +0100

    refactor(repository): refactor GetAllAsync method

--- Architecture.Infrastructure/Todo/TodoItemRepository.cs
+++ Architecture.Infrastructure/Todo/TodoItemRepository.cs
@@ -1,22 +1,21 @@
 ﻿namespace Architecture.Infrastructure.Todo
 {
-    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
     using Architecture.DataSource.Cache;
     using Architecture.DataSource.MongoDb.Todo;
     using Architecture.Domain.Todo;
-    using Architecture.Utils.Extensions;
+
     using LanguageExt;
 
     using Microsoft.Extensions.Caching.Distributed;
     using Microsoft.Extensions.Logging;
+
     using static LanguageExt.Prelude;
 
     public class TodoItemRepository : ITodoItemRepository
     {
         private readonly ITodoItemDataSource _todoItemDataSource;
-        private readonly ILogger _logger;
         private readonly ICache<Seq<TodoItem>> _cache;
 
         public TodoItemRepository(
@@ -26,32 +25,24 @@
         {
             _cache = new RedisCache<Seq<TodoItem>>("TodoItemsCacheKey", cache, logger);
             _todoItemDataSource = todoItemDataSource;
-            _logger = logger;
         }
 
-        public async Task<Either<TodoFailure, Seq<TodoItem>>> GetAllAsync()
-        {
-            var getFromDatabaseIfCacheIsNone =
-                fun(
-                    (Option<Seq<TodoItem>> cacheItems) =>
-                        cacheItems.Match(
-                            items => Right(items),
-                            RetrieveFromDatabaseAndFillCache)
-                );
-
-            return (await GetFromCacheAsync())
-                .Bind(getFromDatabaseIfCacheIsNone);
-        }
+        public async Task<Either<TodoFailure, Seq<TodoItem>>> GetAllAsync() =>
+            await GetFromCacheAsync()
+                .BindT(cacheOpt =>
+                    cacheOpt.Match(
+                        xs => Right(xs),
+                        RetrieveFromDatabaseAndFillCache));
 
         public async Task<Either<TodoFailure, Option<TodoItem>>> GetByIdAsync(TodoId id) =>
             await GetAllAsync().MapT(xs => xs.Find(x => x.Id == id));
 
         public async Task<Either<TodoFailure, Unit>> AddAsync(TodoItem item) =>
-                from cache in await GetAllAsync()
-                from _1 in PersistToDatabase(item)
-                from updatedCache in Right(cache.Add(item))
-                from _2 in UpdateCache(updatedCache)
-                select _2;
+            from cache in await GetAllAsync()
+            from _1 in PersistToDatabase(item)
+            from updatedCache in Right(cache.Add(item))
+            from _2 in UpdateCache(updatedCache)
+            select _2;
 
         private Either<TodoFailure, Seq<TodoItem>> RetrieveFromDatabaseAndFillCache() =>
             from dbResult in GetFromDatabase()
