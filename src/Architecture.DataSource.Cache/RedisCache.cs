900b605cf514d79888271924aff1d0a481f157ac
KiritchoukC <clement.kiritchouk@gmail.com>
2020-11-21 14:56:51 +0100

    refactor(cache): remove cache helper

--- src/Architecture.DataSource.Cache/RedisCache.cs
+++ src/Architecture.DataSource.Cache/RedisCache.cs
@@ -1,5 +1,9 @@
 ﻿namespace Architecture.DataSource.Cache
 {
+    using System;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
     using Architecture.Domain.Common.Cache;
     using Architecture.Utils.Extensions;
 
@@ -7,6 +11,9 @@
 
     using Microsoft.Extensions.Caching.Distributed;
     using Microsoft.Extensions.Logging;
+
+    using static Architecture.Utils.Constructors.Constructors;
+    using static LanguageExt.Prelude;
 
     public class RedisCache<T> : ICache<T>
     {
@@ -25,11 +32,29 @@
         }
 
         public EitherAsync<CacheFailure, Option<T>> GetAsync() =>
-            CacheHelper.GetBytes(() => _cache.GetStringAsync(_cacheKey))
-                .MapO(CacheHelper.DeserializeStringToObject<T>);
+            TryAsync(async () => (await _cache.GetStringAsync(_cacheKey)).Apply(Optional))
+                .ToEither()
+                .MapLeft(CacheFailureCon.Fetch)
+                .MapO(DeserializeStringToObject);
 
         public EitherAsync<CacheFailure, Unit> SetAsync(T item)
-            => CacheHelper.SerializeObjectToString(item)
-                .Bind(jsonStr => CacheHelper.SetBytes(() => _cache.SetStringAsync(_cacheKey, jsonStr)));
+            => SerializeObjectToString(item)
+                .Bind(SetBytes(jsonStr => _cache.SetStringAsync(_cacheKey, jsonStr)));
+
+        private static EitherAsync<CacheFailure, string> SerializeObjectToString(T item) =>
+                Try(() => JsonSerializer.Serialize(item))
+                    .ToEitherAsync()
+                    .MapLeft(CacheFailureCon.Serialization);
+
+        private static EitherAsync<CacheFailure, T> DeserializeStringToObject(string jsonString) =>
+            Try(() => JsonSerializer.Deserialize<T>(jsonString) ?? throw new Exception("Deserialized object is null"))
+                .ToEitherAsync()
+                .MapLeft(CacheFailureCon.Deserialization);
+
+        private static Func<string, EitherAsync<CacheFailure, Unit>> SetBytes(Func<string, Task> cacheSet) =>
+            jsonString =>
+            TryAsync(() => cacheSet(jsonString).ToUnit())
+                .ToEither()
+                .MapLeft(CacheFailureCon.Insert);
     }
 }
